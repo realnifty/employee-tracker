b5596435b682865dc4998f33101b65dbc6bdb705
gordo <rogelioalbertopedraza@gmail.com>
2022-05-23 19:18:18 -0500

    view employees fuinction

--- server.js
+++ server.js
@@ -83,6 +83,25 @@ viewRoles = () => {
     });
 };
 
+viewEmps = () => {
+    const sql = `SELECT employee.id, 
+                      employee.first_name, 
+                      employee.last_name, 
+                      role.title, 
+                      department.name AS department,
+                      role.salary, 
+                      CONCAT (manager.first_name, " ", manager.last_name) AS manager
+                      FROM employee
+                      LEFT JOIN role ON employee.role_id = role.id
+                      LEFT JOIN department ON role.department_id = department.id
+                      LEFT JOIN employee manager ON employee.manager_id = manager.id`;
+    db.query(sql, (err, rows) => {
+        if (err) throw err;
+        console.table(rows);
+        userPrompt();
+    });
+};
+
 userPrompt();
 
 db.connect(err => {
